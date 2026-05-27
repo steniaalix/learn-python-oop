be7c2bb4e656fd191daf38dd1b49b6e25e403979
Steni <steniaalix@gmail.com>
2026-05-27 22:20:08 +0530

    Learned statisics in statistics_module.py and sys in sys_module.py

---
+++ statistics_module.py
@@ -0,0 +1,19 @@
+import statistics as stat
+marks=[90,80,100,70]
+
+'''
+print(stat.mean(marks))
+
+print(stat.median(marks))
+
+nums=[1,2,3,4,3]
+print(stat.mode(nums))
+
+print(stat.variance(marks))
+
+print(stat.stdev(marks))
+'''
+print(f"Mean: {stat.mean(marks)}")
+print(f"Median: {stat.median(marks)}")
+print(f"Mode: {stat.mode(marks)}")
+
---
+++ sys_module.py
@@ -0,0 +1,37 @@
+import sys
+'''
+print(sys.version)
+
+print(sys.platform)
+
+
+print("Start")
+sys.exit()
+print("Never runs")
+
+for i in range(10):
+    if i==7:
+        sys.exit()
+    print(i)
+print("hello world")
+
+x=[1,2,3]
+print(sys.getsizeof(x))
+
+name=sys.argv[1]
+print(f"Hello {name}")
+
+
+print(sys.argv)
+
+print(sys.path)
+
+print(sys.getrecursionlimit())
+
+print(sys.version)
+print(sys.platform)
+
+[a,name,age]=sys.argv
+print(f"Name: {name}")
+print(f"Age : {age}")
+'''
